86a0def755c7a1610e78bc9f2a89cc0705260067
Thiago Moura Witt <thiago.witt@gmail.com>
2015-02-01 21:29:59 -0200

    Refatoração e novas opções de consulta.
    - Estado e cidade parametrizados
    - Consulta periódica
    - Refatoração

--- agendaPassaporte.py
+++ agendaPassaporte.py
@@ -1,72 +1,116 @@
-#!/opt/local/bin/python2.7
-"""
-GET https://www7.dpf.gov.br/sinpa/realizarReagendamento.do?dispatcher=exibirSolicitacaoReagendamento&validate=false
-
-POST https://www7.dpf.gov.br/sinpa/realizarReagendamento.do
-dispatcher=processarConsultaAgendamento&validate=true&origem=exibirSolicitacaoAgendamento&operacao=reagendar&cpf=302.131.598-48&protocolo=&dataNascimento=30%2F09%2F1981&senha=xndm7n
-
-POST https://www7.dpf.gov.br/sinpa/realizarReagendamento.do
-dispatcher=exibirInclusaoAgendamento&postoId=1375&validate=false&ufPosto=SP&cidadePosto=7107
-"""
+#!/usr/bin/python
+# -*- coding: UTF8 -*-
 
 import re
 import requests
 import StringIO
+import random
+import sys
+import time
 from PIL import Image
 from lxml import html
+from datetime import datetime
+from dateutil import parser
 
-cpf = raw_input("Digite o CPF:")
-dtNasc = raw_input("Digite a data de nascimento:")
+def carregarMunicipios(uf):
+	params = {
+		"dispatcher": "carregaMunicipio",
+		"dummy": random.randint(10000000, 99999999),
+		"ufPosto": uf,
+		"utilizaCidadeSessao": "false"
+	}
+	r = requests.get("https://www7.dpf.gov.br/sinpa/realizarAgendamento.do", params=params, verify=False)
+	tree = html.fromstring(r.content)
+	cidadesElem = tree.xpath("//option")
+	return [{"id": x.get("value"), "name": x.text} for x in cidadesElem]
 
-s = requests.Session()
-r = s.get("https://www7.dpf.gov.br/sinpa/realizarReagendamento.do?dispatcher=exibirSolicitacaoReagendamento&validate=false", verify=False)
-#print r.text
+def carregarPostos(cidade):
+	params = {
+		"dispatcher": "carregaPostos",
+		"dummy": random.randint(10000000, 99999999),
+		"municipio": cidade
+	}
+	r = requests.get("https://www7.dpf.gov.br/sinpa/realizarAgendamento.do", params=params, verify=False)
+	tree = html.fromstring(r.content)
+	postosElem = tree.xpath("//a")
+	return [{"id": re.sub(r"[^\d]+", r"", x.get("href")), "name": x.text} for x in postosElem]
 
-print "Buscando captcha\n\n\n"
+def iniciarSessao(cpf, dtNasc):
+	s = requests.Session()
+	r = s.get("https://www7.dpf.gov.br/sinpa/realizarReagendamento.do?dispatcher=exibirSolicitacaoReagendamento&validate=false", verify=False)
+	r = s.get("https://www7.dpf.gov.br/sinpa/jcaptcha.do", verify=False)
 
-r = s.get("https://www7.dpf.gov.br/sinpa/jcaptcha.do", verify=False)
+	im = Image.open(StringIO.StringIO(r.content))
+	im.save("captcha.jpg", "JPEG")
+	im.show()
 
-im = Image.open(StringIO.StringIO(r.content))
-im.save("captcha.jpg", "JPEG")
-im.show()
+	captcha = raw_input("Digite o captcha:")
 
-captcha = raw_input("Digite o captcha:")
-
-print "Validando o captcha\n\n\n"
-
-payload = {
-	"dispatcher": "processarConsultaAgendamento", 
-	"validate": "true",
-	"origem": "exibirSolicitacaoAgendamento",
-	"operacao": "reagendar",
-	"cpf": cpf,
-	"protocolo": None,
-	"dataNascimento": dtNasc,
-	"senha": captcha
-}
-
-r = s.post("https://www7.dpf.gov.br/sinpa/realizarReagendamento.do", data=payload, verify=False)
-tree = html.fromstring(r.content)
-postosElem = tree.xpath("//span[@id='postos']/a")
-
-for posto in postosElem:
-	postoId = re.sub(r"[^\d]+", r"", posto.get("href"))
-	postoName = posto.text.strip().split("\n")[0].strip().rstrip("-")
-	print "\nPosto: %s - %s" % (postoId, postoName)
+	print "Validando captcha"
 
 	payload = {
-		"dispatcher": "exibirInclusaoAgendamento",
-		"postoId": postoId,
-		"validate": "false",
-		"ufPosto": "SP",
-		"cidadePosto": "7107"
+		"dispatcher": "processarConsultaAgendamento", 
+		"validate": "true",
+		"origem": "exibirSolicitacaoAgendamento",
+		"operacao": "reagendar",
+		"cpf": cpf,
+		"protocolo": None,
+		"dataNascimento": dtNasc,
+		"senha": captcha
 	}
 
 	r = s.post("https://www7.dpf.gov.br/sinpa/realizarReagendamento.do", data=payload, verify=False)
 	tree = html.fromstring(r.content)
+	postosElem = tree.xpath("//span[@id='postos']/a")
+	return None if (len(postosElem) == 0) else s
+
+def carregarDatas(session, uf, cidade, posto):
+	payload = {
+		"dispatcher": "exibirInclusaoAgendamento",
+		"postoId": posto,
+		"validate": "false",
+		"ufPosto": uf,
+		"cidadePosto": cidade
+	}
+
+	r = session.post("https://www7.dpf.gov.br/sinpa/realizarReagendamento.do", data=payload, verify=False)
+	tree = html.fromstring(r.content)
+	#print r.content
 	datas = tree.xpath("//option")
+	print datas[0].get("value")
+	return [parser.parse(x.get("value"), dayfirst=True) for x in datas if len(x.get("value").strip()) > 0]
 
-	for data in datas:
-		print "%s - %s" % (data.get("value"), data.text)
 
-print
+uf = raw_input("Digite seu estado (UF):").upper()
+cidades = carregarMunicipios(uf)
+print cidades[10]
+
+for i in range(0, len(cidades)):
+	print "%2s) %s" % (i+1, cidades[i]["name"])
+
+cidadeIndex = raw_input("Digite o número da cidade: ")
+cidade = cidades[int(cidadeIndex)-1]
+postos = carregarPostos(cidade["id"])
+
+cpf = raw_input("Digite o CPF: ")
+dtNasc = raw_input("Digite a data de nascimento: ")
+
+s = iniciarSessao(cpf, dtNasc)
+if not s:
+	print "Falha ao iniciar sessão"
+	sys.exit()
+
+print "Sessão iniciada com sucesso.\n"
+
+while True:
+	print datetime.now()
+	print "Iniciando consultas\n"
+	for posto in postos:
+		print "Posto %s" % posto["name"]
+		print "Datas disponíveis:"
+		datas = carregarDatas(s, uf, cidade["id"], posto["id"])
+		for data in datas:
+			print data.strftime("%d/%m/%Y")
+		print
+	print "\n\n"
+	time.sleep(300)
